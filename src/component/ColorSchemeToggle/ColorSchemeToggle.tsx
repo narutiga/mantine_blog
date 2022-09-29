c9a343d244ebbd0263d3c75dd191c38f15979bbc
narutiga <100295602+narutiga@users.noreply.github.com>
2022-09-29 19:49:24 +0900

    ✨ ColorSchemeToggleを作成

---
+++ src/component/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -0,0 +1,32 @@
+import { useMantineColorScheme, ActionIcon, Group } from "@mantine/core";
+import { IconSun, IconMoonStars } from "@tabler/icons";
+
+/** @package */
+export const ColorSchemeToggle = () => {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+
+  return (
+    <Group position="center" my="xl">
+      <ActionIcon
+        onClick={() => toggleColorScheme()}
+        size="lg"
+        sx={(theme) => ({
+          backgroundColor:
+            theme.colorScheme === "dark"
+              ? theme.colors.dark[6]
+              : theme.colors.gray[0],
+          color:
+            theme.colorScheme === "dark"
+              ? theme.colors.red[3]
+              : theme.colors.indigo[4],
+        })}
+      >
+        {colorScheme === "dark" ? (
+          <IconSun size={18} />
+        ) : (
+          <IconMoonStars size={18} />
+        )}
+      </ActionIcon>
+    </Group>
+  );
+};
