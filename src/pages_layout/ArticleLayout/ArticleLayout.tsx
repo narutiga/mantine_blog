6cf43931fb7391af224048d99247b62782aaf87f
narutiga <100295602+narutiga@users.noreply.github.com>
2022-10-01 00:12:01 +0900

    💄 フッターを下に固定

--- src/pages_layout/ArticleLayout/ArticleLayout.tsx
+++ src/pages_layout/ArticleLayout/ArticleLayout.tsx
@@ -1,10 +1,14 @@
 import { CustomLayout } from "next";
+import { Footer } from "src/component/Footer";
+import { HeaderMenu } from "src/component/Header";
 
 /** @package */
 export const ArticleLayout: CustomLayout = (page) => {
   return (
-    <div className="font-mono">
-      <main className="w-5/6 mx-auto">{page}</main>
+    <div className="flex min-h-screen flex-col font-mono">
+      <HeaderMenu />
+      <main className="flex flex-1 flex-col w-5/6 pt-20 mx-auto">{page}</main>
+      <Footer />
     </div>
   );
 };
