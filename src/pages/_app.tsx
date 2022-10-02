44435805d881002090f8d4a606523d91eec37e7f
narutiga <100295602+narutiga@users.noreply.github.com>
2022-10-02 18:27:51 +0900

    ✨ ファビコン作成

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -28,13 +28,22 @@ const App: CustomAppPage = ({ Component, pageProps }) => {
       <MantineProvider
         withGlobalStyles
         withNormalizeCSS
-        theme={{ colorScheme }}
+        theme={{ fontFamily: "monospace", colorScheme }}
       >
         <Head>
           <title>はじめてのMantine</title>
           <meta
             name="description"
             content="初学者による初学者のためのMantineブログです。"
+          />
+          <link
+            rel="icon"
+            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 style=%22dominant-baseline:central;text-anchor:middle;font-size:90px;%22>🔰</text></svg>"
+          ></link>
+          <link
+            rel="icon alternate"
+            type="image/png"
+            href="https://twemoji.maxcdn.com/v/14.0.2/72x72/1f530.png"
           />
         </Head>
         {getLayout(<Component {...pageProps} />)}
